8ebba30da65e1c7367e63e8220e27bef72a16c00
user\KITCOOP <tlsaudwl2@naver.com>
2024-07-30 13:42:20 +0900

    update

--- stockSage/settings.py
+++ stockSage/settings.py
@@ -31,7 +31,8 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "stockSage.urls"
